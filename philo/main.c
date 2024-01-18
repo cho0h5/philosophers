0744f927ecdcb6860b1336cb5bb6bfaf27c2280e
Youngho Cho <younghoc@c2r5s6.42seoul.kr>
2024-01-18 16:43:43 +0900

    test: gettimeofday

--- philo/main.c
+++ philo/main.c
@@ -6,12 +6,14 @@
 /*   By: Youngho Cho <younghoc@student.42seoul.kr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/18 16:01:54 by Youngho Cho       #+#    #+#             */
-/*   Updated: 2024/01/18 16:18:38 by Youngho Cho      ###   ########.fr       */
+/*   Updated: 2024/01/18 16:40:21 by Youngho Cho      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <pthread.h>
+#include <stdio.h>
+#include <sys/time.h>
 
 void	*philosophers(void *arg)
 {
@@ -26,11 +28,15 @@ int	main(void)
 	pthread_t		p1;
 	pthread_t		p2;
 	pthread_mutex_t	mutex;
+	struct timeval	time;
 
+	gettimeofday(&time, NULL);
+	printf("%ld %d\n", time.tv_sec, time.tv_usec);
 	pthread_mutex_init(&mutex, NULL);
 	pthread_create(&p1, NULL, philosophers, &mutex);
 	pthread_create(&p2, NULL, philosophers, &mutex);
 	pthread_join(p1, NULL);
 	pthread_join(p2, NULL);
+	pthread_mutex_destroy(&mutex);
 	return (0);
 }
