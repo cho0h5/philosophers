2c7bceb979d0b79c8d64bd8d37b8bf955db9bf36
Youngho Cho <dominoyh5@gmail.com>
2024-02-11 17:58:55 +0900

    add: mutex to print*()

--- philo/print.c
+++ philo/print.c
@@ -14,25 +14,35 @@
 
 void	print_fork(int id, t_env *env)
 {
+	pthread_mutex_lock(&env->mutex_print);
 	printf("%lld %d has taken a fork\n", get_time_in_ms() - env->start_time, id + 1);
+	pthread_mutex_unlock(&env->mutex_print);
 }
 
 void	print_eating(int id, t_env *env)
 {
+	pthread_mutex_lock(&env->mutex_print);
 	printf("%lld %d is eating\n", get_time_in_ms() - env->start_time, id + 1);
+	pthread_mutex_unlock(&env->mutex_print);
 }
 
 void	print_sleeping(int id, t_env *env)
 {
+	pthread_mutex_lock(&env->mutex_print);
 	printf("%lld %d is sleeping\n", get_time_in_ms() - env->start_time, id + 1);
+	pthread_mutex_unlock(&env->mutex_print);
 }
 
 void	print_thinking(int id, t_env *env)
 {
+	pthread_mutex_lock(&env->mutex_print);
 	printf("%lld %d is thinking\n", get_time_in_ms() - env->start_time, id + 1);
+	pthread_mutex_unlock(&env->mutex_print);
 }
 
 void	print_died(int id, t_env *env)
 {
+	pthread_mutex_lock(&env->mutex_print);
 	printf("%lld %d died\n", get_time_in_ms() - env->start_time, id + 1);
+	pthread_mutex_unlock(&env->mutex_print);
 }
