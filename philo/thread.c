4c1ccd97cfdf4ea88b2f62e35f0558913b6a9c8d
Youngho Cho <dominoyh5@gmail.com>
2024-02-13 13:49:16 +0900

    update: change parameter type

--- philo/thread.c
+++ philo/thread.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   thread.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: younghoc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: younghoc <younghoc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 20:15:56 by younghoc          #+#    #+#             */
-/*   Updated: 2024/02/12 20:15:57 by younghoc         ###   ########.fr       */
+/*   Updated: 2024/02/13 13:32:25 by younghoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,24 +15,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
-static void	wait_to_start(t_parameter const *param)
+static void	wait_to_start(t_parameter *const param)
 {
 	pthread_mutex_lock(&param->env->mutex_ready);
 	pthread_mutex_unlock(&param->env->mutex_ready);
 }
 
-static void	check_me_starve(t_parameter const *param)
+static void	check_me_starve(t_parameter *const param)
 {
 	if (get_time() - param->env->start_time - param->last_eat_time > param->env->time_to_die)
 	{
 		pthread_mutex_lock(&param->env->mutex_starve);
 		param->env->is_someone_starved = 1;
 		pthread_mutex_unlock(&param->env->mutex_starve);
-		printf("id%d: died\n", param->id);
+		printf("%lld\t%d\tdied\n",
+			(get_time() - param->env->start_time) / 1000, param->id);
 	}
 }
 
-static int	check_someone_starve(t_parameter const *param)
+static int	check_someone_starve(t_parameter *const param)
 {
 	pthread_mutex_lock(&param->env->mutex_starve);
 	if (param->env->is_someone_starved)
@@ -44,64 +45,75 @@ static int	check_someone_starve(t_parameter const *param)
 	return (0);
 }
 
-static t_fork	*left_fork(t_parameter const *param)
+static t_fork	*left_fork(t_parameter *const param)
 {
 	const int	id_fork = param->id;
 
 	return (&param->env->forks[id_fork]);
 }
 
-static t_fork	*right_fork(t_parameter const *param)
+static t_fork	*right_fork(t_parameter *const param)
 {
 	const int	id_fork = (param->id + 1) % param->env->number_of_philosophers;
 
 	return (&param->env->forks[id_fork]);
 }
 
-static int	take_forks(t_parameter const *param)
+static int	take_forks(t_parameter *const param)
 {
 	while (try_lock_fork(left_fork(param)))
 	{
 		check_me_starve(param);
 		if (check_someone_starve(param))
-		{
 			return (-1);
-		}
 	}
-	printf("id%d: has taken left fork (%p)\n", param->id, left_fork(param));
-	usleep(1000);
+	printf("%lld\t%d\thas taken a left fork\n",
+		(get_time() - param->env->start_time) / 1000, param->id);
 	while (try_lock_fork(right_fork(param)))
 	{
 		check_me_starve(param);
 		if (check_someone_starve(param))
-		{
 			return (-1);
-		}
 	}
-	printf("id%d: has taken right fork (%p)\n", param->id, right_fork(param));
+	printf("%lld\t%d\thas taken a right fork\n",
+		(get_time() - param->env->start_time) / 1000, param->id);
 	return (0);
 }
 
-static void	release_forks(t_parameter const *param)
+static void	release_forks(t_parameter *const param)
 {
 	unlock_fork(left_fork(param));
 	unlock_fork(right_fork(param));
 }
 
-//static int	eat(t_parameter const *param)
-//{
-//}
+static int	eat(t_parameter *const param)
+{
+	const long long	start_time_eat = get_time();
+
+	while (get_time() - start_time_eat < param->env->time_to_eat)
+	{
+		check_me_starve(param);
+		if (check_someone_starve(param))
+			return (-1);
+		// usleep(100);
+	}
+	param->last_eat_time = get_time();
+	return (0);
+}
 
 void	*philosopher(void *arg)
 {
-	t_parameter const	*param = arg;
+	t_parameter *const	param = arg;
 
 	wait_to_start(param);
-	printf("id%d: %lld\n", param->id, get_time());
+	printf("%lld\t%d\tcreated\n",
+		(get_time() - param->env->start_time) / 1000, param->id);
 	if (take_forks(param))
 		return (NULL);
-//	if (eat(param))
-//		return (NULL);
+	printf("%lld\t%d\tis eating\n",
+		(get_time() - param->env->start_time) / 1000, param->id);
+	if (eat(param))
+		return (NULL);
 	release_forks(param);
 	return (NULL);
 }
