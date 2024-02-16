0bdd8786008eebfd6ad88553b8166333275b77ee
Youngho Cho <dominoyh5@gmail.com>
2024-02-16 18:54:07 +0900

    add eat.c, sleep.c

---
+++ philo_bonus/philosopher_eat_bonus.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher_eat_bonus.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: younghoc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 18:50:35 by younghoc          #+#    #+#             */
+/*   Updated: 2024/02/16 18:50:36 by younghoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_bonus.h"
+#include <unistd.h>
+
+int	check_eat_done(t_parameter *const param)
+{
+	return ((param->env->number_of_must_eat != -1
+			&& param->count_eat >= param->env->number_of_must_eat));
+}
+
+int	philosopher_eat(t_parameter *const param)
+{
+	const long long	start_time_eat = get_time();
+
+	param->last_eat_time = get_time();
+	usleep(0.9 * param->env->time_to_eat);
+	while (get_time() - start_time_eat < param->env->time_to_eat)
+	{
+		check_me_starve(param);
+		if (check_someone_starve(param))
+			return (-1);
+		usleep(100);
+	}
+	return (0);
+}
---
+++ philo_bonus/philosopher_sleep_bonus.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosopher_sleep_bonus.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: younghoc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 18:50:39 by younghoc          #+#    #+#             */
+/*   Updated: 2024/02/16 18:50:40 by younghoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_bonus.h"
+#include <unistd.h>
+
+int	philosopher_sleep(t_parameter *const param)
+{
+	const long long	start_time_sleep = get_time();
+
+	usleep(0.9 * param->env->time_to_sleep);
+	while (get_time() - start_time_sleep < param->env->time_to_sleep)
+	{
+		check_me_starve(param);
+		if (check_someone_starve(param))
+			return (-1);
+		usleep(100);
+	}
+	return (0);
+}
