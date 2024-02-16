dd2e651b9f3c6e89417f5d8da4bad873d39961f5
Youngho Cho <dominoyh5@gmail.com>
2024-02-16 17:41:25 +0900

    add: print_bonus.c

---
+++ philo_bonus/print_bonus.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print_bonus.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: younghoc <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/16 17:31:03 by younghoc          #+#    #+#             */
+/*   Updated: 2024/02/16 17:31:05 by younghoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_bonus.h"
+#include <stdio.h>
+#include <sys/semaphore.h>
+#include <unistd.h>
+
+int	print_fork(t_parameter *param)
+{
+	sem_wait(param->env->sem_print);
+//	while (try_lock_fork(&param->env->fork_print))
+//	{
+//		check_me_starve(param);
+//		if (check_someone_starve(param))
+//			return (-1);
+//		usleep(100);
+//	}
+	printf("%lld %d has taken a fork\n",
+		(get_time() - param->env->start_time) / 1000, param->id + 1);
+//	unlock_fork(&param->env->fork_print);
+	sem_post(param->env->sem_print);
+	return (0);
+}
+
+int	print_eating(t_parameter *param)
+{
+	sem_wait(param->env->sem_print);
+//	while (try_lock_fork(&param->env->fork_print))
+//	{
+//		check_me_starve(param);
+//		if (check_someone_starve(param))
+//			return (-1);
+//		usleep(100);
+//	}
+	printf("%lld %d is eating\n",
+		(get_time() - param->env->start_time) / 1000, param->id + 1);
+//	unlock_fork(&param->env->fork_print);
+	sem_post(param->env->sem_print);
+	return (0);
+}
+
+int	print_sleeping(t_parameter *param)
+{
+	sem_wait(param->env->sem_print);
+//	while (try_lock_fork(&param->env->fork_print))
+//	{
+//		check_me_starve(param);
+//		if (check_someone_starve(param))
+//			return (-1);
+//		usleep(100);
+//	}
+	printf("%lld %d is sleeping\n",
+		(get_time() - param->env->start_time) / 1000, param->id + 1);
+//	unlock_fork(&param->env->fork_print);
+	sem_post(param->env->sem_print);
+	return (0);
+}
+
+int	print_thinking(t_parameter *param)
+{
+	sem_wait(param->env->sem_print);
+//	while (try_lock_fork(&param->env->fork_print))
+//	{
+//		check_me_starve(param);
+//		if (check_someone_starve(param))
+//			return (-1);
+//		usleep(100);
+//	}
+	printf("%lld %d is thinking\n",
+		(get_time() - param->env->start_time) / 1000, param->id + 1);
+//	unlock_fork(&param->env->fork_print);
+	sem_post(param->env->sem_print);
+	return (0);
+}
+
+int	print_died(t_parameter *param)
+{
+	sem_wait(param->env->sem_print);
+//	while (try_lock_fork(&param->env->fork_print))
+//		usleep(100);
+	printf("%lld %d died\n",
+		(get_time() - param->env->start_time) / 1000, param->id + 1);
+	// sem_post(param->env->sem_print);
+	return (0);
+}
